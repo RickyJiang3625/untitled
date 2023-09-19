7e7c85505c13bb8d553dd77a28975c9d9798378b
BT_4N2_28 <ricky123jg@gmail.com>
2023-09-19 13:06:07 -0400

    Added a new line for my favorite food

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 System.out.println("Ricky Jiang");
 System.out.print("My favorite movie is Home Alone");
+System.out.print("My favorite food is Pizza");
     }
 }
