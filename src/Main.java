99d0a9c52636d11a7466671269dd05b7eb95385e
RickyJiang3625 <145377795+RickyJiang3625@users.noreply.github.com>
2025-06-05 21:06:12 +0000

    ?

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game(Arrays.asList("Alice", "Bob"));
+        Game game = new Game(Arrays.asList("Tim", "Bob"));
         while (true) {
             game.playRound();
             try { Thread.sleep(1000); } catch (Exception e) {}
